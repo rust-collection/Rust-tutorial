5b13f5311dec41bc14e3d8896228711d3c2cf02b
Subranium <supeng842499467@gmail.com>
2021-04-21 09:12:04 +0800

    Delete code/hello-cargo/target directory

--- code/hello-cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-/Users/super/develop/Rust-tutorial/code/hello-cargo/target/debug/deps/hello_cargo: src/main.rs
-
-/Users/super/develop/Rust-tutorial/code/hello-cargo/target/debug/deps/hello_cargo.d: src/main.rs
-
-src/main.rs:
--- code/hello-cargo/target/debug/hello-cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/super/develop/Rust-tutorial/code/hello-cargo/target/debug/hello-cargo: /Users/super/develop/Rust-tutorial/code/hello-cargo/src/main.rs
