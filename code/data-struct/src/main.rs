05cfbf78248a78536f8f02b87f91f5a68f10c7b4
super <supeng842499467@gmail.com>
2021-05-24 20:47:48 +0800

    update

--- code/data-struct/src/main.rs
+++ code/data-struct/src/main.rs
@@ -1,3 +1,19 @@
+struct Person{
+    name: String,
+    age: u8,
+    likes_oranges: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let person = Person{
+        name: String::from("Adam"),
+        likes_oranges: true,
+        age: 25
+    };
+
+    if person.likes_oranges {
+        println!("{:?} is {:?} and likes oranges.", person.name, person.age);  
+    } else {
+        println!("{:?} is {:?} and doesn't like oranges.", person.name, person.age);  
+    }
 }
