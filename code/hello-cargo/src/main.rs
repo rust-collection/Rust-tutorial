47254d56fd18ef4577d9f7127ce64558d9c25660
super <supeng842499467@gmail.com>
2021-06-13 13:48:10 +0800

    update

--- code/hello-cargo/src/main.rs
+++ code/hello-cargo/src/main.rs
@@ -1,4 +1,9 @@
+// 在数字中间加入下划线增强可读性
+const MAX_POINTS: u32 = 1000_000;
+
 fn main() {
+
+    println!("max_points: {}", MAX_POINTS);
     let mut a = 10;
     let b = true;
     println!("a is {}, b is {}", a, b);
