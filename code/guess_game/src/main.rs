fd9e4e36122e303f686d1e9b411a0f39d237b6d7
super <supeng842499467@gmail.com>
2021-06-13 13:29:03 +0800

    update

--- code/guess_game/src/main.rs
+++ code/guess_game/src/main.rs
@@ -1,3 +1,29 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("guss number！");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("please guess a number!");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("can not read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("you guess number is: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Equal => {
+                println!("you win!!");
+                break;
+            }
+        }
+    }
 }
