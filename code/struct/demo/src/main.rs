e9675b690e979b5593066b3fea0c1e1c911d95d8
super <supeng842499467@gmail.com>
2021-06-22 09:54:58 +0800

    update

---
+++ code/struct/demo/src/main.rs
@@ -0,0 +1,20 @@
+// 使结构体派生于Debug，使其可以格式化输出
+#[derive(Debug)]
+struct Rectangle{
+    width:u32,
+    length:u32,
+}
+
+fn area(rec: &Rectangle) -> u32{
+    rec.width * rec.length
+}
+
+fn main() {
+    let rec = Rectangle{
+        width:30,
+        length:50,
+    };
+    println!("{}", area(&rec));
+
+    println!("{:#?}", rec)
+}
