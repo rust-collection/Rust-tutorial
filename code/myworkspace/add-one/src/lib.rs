cf750c122bee88295c1f3cca48389902aa95a6ca
super <supeng842499467@gmail.com>
2022-03-28 20:21:25 +0800

    update

---
+++ code/myworkspace/add-one/src/lib.rs
@@ -0,0 +1,18 @@
+//! # add-one crate
+//!
+//! `add-one crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = add_one::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
